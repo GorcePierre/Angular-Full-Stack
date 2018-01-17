5a2997b69373b56febf6b29b6e817b5f6ee0229b
Victor Leduc <victor@wildcodeschool.fr>
2018-01-17 10:48:42 +0100

    Backend : Setup express-jwt and check role of user for each

--- server/routes.ts
+++ server/routes.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as jwt from 'express-jwt';
 
 import CatCtrl from './controllers/cat';
 import UserCtrl from './controllers/user';
@@ -9,13 +10,35 @@ export default function setRoutes(app) {
 
   const router = express.Router();
 
+  const auth = jwt({
+    secret: process.env.SECRET_TOKEN,
+    requestProperty: 'auth',
+    credentialsRequired: false
+  });
+
+  const isLogged = (req, res, next) => {
+    if (!req.auth || !req.auth.user) {
+      res.status(401).send('invalid token');
+    } else {
+      next();
+    }
+  };
+
+  const isAdmin = (req, res, next) => {
+    if (!req.auth || !req.auth.user || req.auth.user.role != 'admin') {
+      res.status(403).send('unauthorized');
+    } else {
+      next();
+    }
+  };
+
   const catCtrl = new CatCtrl();
   const userCtrl = new UserCtrl();
 
   // Cats
-  router.route('/cats').get(catCtrl.getAll);
+  router.route('/cats').get(isLogged, catCtrl.getAll);
   router.route('/cats/count').get(catCtrl.count);
-  router.route('/cat').post(catCtrl.insert);
+  router.route('/cat').post(isAdmin, catCtrl.insert);
   router.route('/cat/:id').get(catCtrl.get);
   router.route('/cat/:id').put(catCtrl.update);
   router.route('/cat/:id').delete(catCtrl.delete);
@@ -30,6 +53,6 @@ export default function setRoutes(app) {
   router.route('/user/:id').delete(userCtrl.delete);
 
   // Apply the routes to our application with the prefix /api
-  app.use('/api', router);
+  app.use('/api', auth, router);
 
 }
